6794b3798e2df1c9bff5a4f32904a06c1b9f3f14
zhuxiangfa <zhuxiangfa@huya.com>
2022-03-23 15:45:39 +0800

    chore:更多列表备份

--- src/components/WelcomeMain/ResourceAnnounce/index.tsx
+++ src/components/WelcomeMain/ResourceAnnounce/index.tsx
@@ -1,13 +1,14 @@
-import { Card, Col, Empty, Row, Typography } from "antd"
-import { useState } from "react"
+import { Card, Col, Divider, Empty, Row, Typography } from "antd"
+import { memo, useEffect, useState } from "react"
 import { ToolOutlined, DoubleRightOutlined } from '@ant-design/icons';
 import styles from './index.less';
 import { getConsultationList, getPurchaseAnnouncementList, getResultAnnouncementList, getCorrectAnnouncementList } from '@/services/resource/api'
 import { useRequest } from '@umijs/hooks';
 import classnames from 'classnames'
 import { Link } from "umi";
+import { history } from "umi";
 
-const ResourceAnnounce: React.FC = () => {
+const ResourceAnnounce: React.FC = (props: any) => {
     /**
      * 渲染具体内容
      */
@@ -55,6 +56,44 @@ const ResourceAnnounce: React.FC = () => {
     }
 
     /**
+     * 加载更多
+     */
+    const ResourceItemElemMore = ({ data, state }: any) => {
+
+        let handleData = data // 取前8个
+        return (
+            <div style={{ width: 710 }}>
+                {
+                    handleData?.length ? handleData.map((item: any, index: any) => {
+                        return (
+                            <div key={item.id} className={styles.resourceItemMore}>
+                                <Row>
+                                    <Col span={1}>
+                                        [{index + 1}]
+                                    </Col>
+                                    <Col span={13} className={styles.itemHoverMore}>
+                                        <Link to={`/resourceDetail?current=${state}&id=${item.id}`}>
+                                            {item.name}
+                                        </Link>
+                                    </Col>
+                                    <Col span={4} style={{ color: '#ff4d4f', fontSize: 12 }}>
+                                        活动已结束
+                                    </Col>
+                                    <Col span={4} style={{ fontSize: 12 }}>
+                                        {item.publishTime}
+                                    </Col>
+                                </Row>
+                                <Divider dashed />
+                            </div>
+                        )
+                    })
+                        :
+                        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                }
+            </div>)
+    }
+
+    /**
      * 获取公告资源数据
      */
     // 获取征询意见列表
@@ -78,34 +117,54 @@ const ResourceAnnounce: React.FC = () => {
     });
     const correctAnnouncement = correctAnnouncementRes?.data?.data
 
-
+    // 判断是第几个tab
     const [state, setState] = useState(1)
+    // 更多页面的组件
+    const [moreList, setMoreList] = useState(false)
 
     const ResourceContent = () => {
-        let constent = <ResourceItemElem data={consultationList} state={state} />
+        let content = <ResourceItemElem data={consultationList} state={state} />
 
         if (state === 1) {
-            constent = <ResourceItemElem data={consultationList} state={state} />
+            content = <ResourceItemElem data={consultationList} state={state} />
         }
         else if (state === 2) {
-            constent = <ResourceItemElem data={purchaseAnnouncement} state={state} />
+            content = <ResourceItemElem data={purchaseAnnouncement} state={state} />
         }
         else if (state === 3) {
-            constent = <ResourceItemElem data={resultAnnouncement} state={state} />
+            content = <ResourceItemElem data={resultAnnouncement} state={state} />
         }
         else if (state === 4) {
-            constent = <ResourceItemElem data={correctAnnouncement} state={state} />
+            content = <ResourceItemElem data={correctAnnouncement} state={state} />
         }
-        return constent
+        return content
     }
+
+    const ResourceContentMore = () => {
+        let content = <ResourceItemElemMore data={consultationList} state={state} />
+        if (state === 1) {
+            content = <ResourceItemElemMore data={consultationList} state={state} />
+        }
+        else if (state === 2) {
+            content = <ResourceItemElemMore data={purchaseAnnouncement} state={state} />
+        }
+        else if (state === 3) {
+            content = <ResourceItemElemMore data={resultAnnouncement} state={state} />
+        }
+        else if (state === 4) {
+            content = <ResourceItemElemMore data={correctAnnouncement} state={state} />
+        }
+        return content
+    }
+
     return (
         <>
-            <Row className={styles.title}>
+            {!moreList ? <><Row className={styles.title}>
                 <Col span={5}>
                     <Typography.Title className={styles.typographytitle} level={5} ><ToolOutlined style={{ margin: '0 10px' }} />货物与服务</Typography.Title>
                 </Col>
                 <Col>
-                    <div className={styles.titleNav}>
+                    <Link to='/welcome?list=true' className={styles.titleNav} onClick={() => setMoreList(true)}>
                         <div className={classnames(styles.navItem, state === 1 ? styles.navItemHover : '')} onMouseEnter={() => setState(1)}>
                             <div className={styles.navItemText}>
                                 征询意见
@@ -130,12 +189,47 @@ const ResourceAnnounce: React.FC = () => {
                             {/* 查询<HeaderSearch options={[]} /> */}
                             {/* 查询 */}
                         </div>
-                    </div>
+                    </Link>
                 </Col>
             </Row>
-            <Row>
-                <ResourceContent />
-            </Row>
+                <Row>
+                    <ResourceContent />
+                </Row></> :
+                <div className={styles.moreListWrapper}>
+                    <Card className={styles.moreListNav}>
+                        <Row>
+                            <div>
+                                采购方式：
+                            </div>
+                            <div className={classnames(styles.moreListNavTitle, state == 0 ? styles.navItemSelected : '')} onClick={() => setState(0)}>
+                                全部
+                            </div>
+                            <div className={classnames(styles.moreListNavTitle, state == 1 ? styles.navItemSelected : '')} onClick={() => setState(1)}>
+                                征询意见
+                            </div>
+                            <div className={classnames(styles.moreListNavTitle, state == 2 ? styles.navItemSelected : '')} onClick={() => setState(2)}>
+                                采购公告
+                            </div>
+                            <div className={classnames(styles.moreListNavTitle, state == 3 ? styles.navItemSelected : '')} onClick={() => setState(3)}>
+                                结果公告
+                            </div>
+                            <div className={classnames(styles.moreListNavTitle, state == 4 ? styles.navItemSelected : '')} onClick={() => setState(4)}>
+                                更正公告
+                            </div>
+                        </Row>
+                        <Divider dashed />
+
+                    </Card>
+                    <Card className={styles.moreListContent}>
+                        {/* {JSON.stringify(consultationList)} */}
+                        <ResourceContentMore />
+                    </Card >
+                    {/* <div >
+
+                    </div> */}
+                </div>
+
+            }
         </>
     )
 
