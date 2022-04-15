2061108be6728ac534322e04add3b69740c11174
faziji <zhu799353764@qq.com>
2022-04-15 23:03:32 +0800

    chore:修改代理地址

--- config/proxy.ts
+++ config/proxy.ts
@@ -12,7 +12,7 @@ export default {
     '/api/': {
       // 要代理的地址
       // target: 'https://preview.pro.ant.design',
-      target: 'http://172.21.40.25:3000',
+      target: 'http://localhost:3000',
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
