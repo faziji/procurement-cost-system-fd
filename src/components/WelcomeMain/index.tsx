b5cfd241bec416a4b69a50f2be53c993ae170092
faziji <zhu799353764@qq.com>
2022-04-16 19:18:47 +0800

    fix:修复下载中心不能点击问题

--- src/components/WelcomeMain/index.tsx
+++ src/components/WelcomeMain/index.tsx
@@ -1,114 +1,152 @@
 import React, { memo, useEffect, useState } from 'react';
 import styles from './index.less';
-import CalendarTender from '../CalendarTender'
+import CalendarTender from '../CalendarTender';
 import { Card, Col, Divider, message, Row, Typography } from 'antd';
 import FormNormalLogin from '../FormNormalLogin';
-import { CalendarOutlined, ContainerOutlined, DoubleLeftOutlined, LoginOutlined, MergeCellsOutlined, ToolOutlined } from '@ant-design/icons';
-import ResourceAnnounce from './ResourceAnnounce'
-import ResourceItemElemMore from './ResourceItemElemMore'
+import {
+  CalendarOutlined,
+  ContainerOutlined,
+  DoubleLeftOutlined,
+  LoginOutlined,
+  MergeCellsOutlined,
+  ToolOutlined,
+} from '@ant-design/icons';
+import ResourceAnnounce from './ResourceAnnounce';
+import ResourceItemElemMore from './ResourceItemElemMore';
 import { useRequest } from 'umi';
 import { getPurchaseAnnouncementList } from '@/services/resource/api';
 
-
 const JumpSystemLine = () => {
-    return (
-        <Row>
-            <Col span={8}>
-                <div className={styles.loginFormEntry1} >
-                    <div className={styles.entryWrapper}>
-                        <img className={styles.imagePic} src="http://www2.scut.edu.cn/_upload/tpl/09/04/2308/template2308/img/icon-gys.png" alt="" />
-                        <p>供应商入口</p>
-                    </div>
-                </div>
-            </Col>
-            <Col span={8}>
-                <div className={styles.loginFormEntry2} >
-                    <div className={styles.entryWrapper}>
-                        <img className={styles.imagePic} src="http://www2.scut.edu.cn/_upload/tpl/09/04/2308/template2308/img/icon-ps.png" alt="" />
-                        <p>评审入口</p>
-                    </div>
-                </div>
-            </Col>
-            <Col span={8}>
-                <div className={styles.loginFormEntry3} >
-                    <div className={styles.entryWrapper}>
-                        <img className={styles.imagePic} src="http://www2.scut.edu.cn/_upload/tpl/09/04/2308/template2308/img/icon-ds.png" alt="" />
-                        <p>管理后台</p>
-                    </div>
-                </div>
-            </Col>
-
-        </Row>
-    )
-}
-
-
+  return (
+    <Row>
+      <Col span={8}>
+        <div className={styles.loginFormEntry1}>
+          <div className={styles.entryWrapper}>
+            <img
+              className={styles.imagePic}
+              src="http://www2.scut.edu.cn/_upload/tpl/09/04/2308/template2308/img/icon-gys.png"
+              alt=""
+            />
+            <p>我的信息</p>
+          </div>
+        </div>
+      </Col>
+      <Col span={8}>
+        <div className={styles.loginFormEntry2}>
+          <div className={styles.entryWrapper}>
+            <img
+              className={styles.imagePic}
+              src="http://www2.scut.edu.cn/_upload/tpl/09/04/2308/template2308/img/icon-ps.png"
+              alt=""
+            />
+            <p>我的关注</p>
+          </div>
+        </div>
+      </Col>
+      <Col span={8}>
+        <div className={styles.loginFormEntry3}>
+          <div className={styles.entryWrapper}>
+            <img
+              className={styles.imagePic}
+              src="http://www2.scut.edu.cn/_upload/tpl/09/04/2308/template2308/img/icon-ds.png"
+              alt=""
+            />
+            <p>管理后台</p>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  );
+};
 
 const WelcomeMain: any = () => {
+  // 是否展示login
+  const [loginCardVisiable, setLoginCardVisiable] = useState(true);
+  // 是否是日历点击搜索跳转的内容
+  const [calendarSearch, setCalendarSearch] = useState(false);
+  const [calendarSearchTime, setCalendarSearchTime] = useState(''); // 日历搜索的时间
 
-    // 是否展示login
-    const [loginCardVisiable, setLoginCardVisiable] = useState(true)
-    // 是否是日历点击搜索跳转的内容
-    const [calendarSearch, setCalendarSearch] = useState(false)
-    const [calendarSearchTime, setCalendarSearchTime] = useState('') // 日历搜索的时间
+  const [purchaseAnnouncement, setPurchaseAnnouncement] = useState({});
 
-    const [purchaseAnnouncement, setPurchaseAnnouncement] = useState({})
+  // 获取征询意见列表
+  useEffect(() => {
+    // 之后要传到后台筛选数据
+    getPurchaseAnnouncementList()
+      .then((res) => {
+        setPurchaseAnnouncement(res?.data);
+      })
+      .catch((err) => console.log(err));
+  }, [calendarSearchTime]);
 
-    // 获取征询意见列表
-    useEffect(() => {
-        // 之后要传到后台筛选数据
-        getPurchaseAnnouncementList().then(res => {
-            setPurchaseAnnouncement(res?.data)
-        }).catch(err => console.log(err))
-    }, [calendarSearchTime]);
+  return (
+    <div className={styles.mainWrapper}>
+      <div className={styles.cardContent}>
+        <div className={styles.firstLineBox}>
+          <div className={styles.calendarWrapper}>
+            <Row className={styles.title}>
+              <Typography.Title className={styles.typographytitle} level={5}>
+                <CalendarOutlined style={{ margin: '0 10px' }} />
+                投标日历
+              </Typography.Title>
+            </Row>
+            <Card className={styles.calendarCard}>
+              <CalendarTender
+                setCalendarSearchTime={setCalendarSearchTime}
+                setCalendarSearch={setCalendarSearch}
+              />
+            </Card>
+          </div>
 
-
-    return (
-        <div className={styles.mainWrapper}>
-            <div className={styles.cardContent}>
-
-                <div className={styles.firstLineBox}>
-                    <div className={styles.calendarWrapper}>
-                        <Row className={styles.title}>
-                            <Typography.Title className={styles.typographytitle} level={5} ><CalendarOutlined style={{ margin: '0 10px' }} />投标日历</Typography.Title>
-                        </Row>
-                        <Card className={styles.calendarCard}>
-                            <CalendarTender setCalendarSearchTime={setCalendarSearchTime} setCalendarSearch={setCalendarSearch} />
-                        </Card>
-                    </div>
-
-                    {/* 不是通过日历搜索 */}
-                    {!calendarSearch && <div className={styles.resourceWrapper}>
-                        <ResourceAnnounce onLoginCardVisiable={setLoginCardVisiable} />
-                    </div>}
-                    {loginCardVisiable && !calendarSearch && <div className={styles.loginWrapper}>
-                        <Row className={styles.title}>
-                            <Typography.Title className={styles.typographytitle} level={5} ><LoginOutlined style={{ margin: '0 10px' }} />用户登录</Typography.Title>
-                        </Row>
-                        <Card className={styles.loginCard}>
-                            <FormNormalLogin />
-                            <JumpSystemLine />
-                        </Card>
-                    </div>}
-                    {/* 通过日历搜索 */}
-                    {calendarSearch && <>
-                        <div className={styles.calendarSearchWrapper}>
-                            <Card className={styles.calendarSearchTitle}>
-                                <Row>
-                                    <p><ContainerOutlined style={{ fontSize: 22, marginRight: 10 }} />{calendarSearchTime}投标项目</p>
-                                    <p className={styles.calendarSearchTitleText} onClick={() => window.location.reload()}> <DoubleLeftOutlined />返回</p>
-                                </Row>
-                                <Divider dashed />
-                            </Card>
-                            <Card className={styles.calendarSearchContent}>
-                                <ResourceItemElemMore data={purchaseAnnouncement} />
-                            </Card>
-                        </div>
-                    </>}
-                </div>
-
+          {/* 不是通过日历搜索 */}
+          {!calendarSearch && (
+            <div className={styles.resourceWrapper}>
+              <ResourceAnnounce onLoginCardVisiable={setLoginCardVisiable} />
             </div>
+          )}
+          {loginCardVisiable && !calendarSearch && (
+            <div className={styles.loginWrapper}>
+              <Row className={styles.title}>
+                <Typography.Title className={styles.typographytitle} level={5}>
+                  <LoginOutlined style={{ margin: '0 10px' }} />
+                  用户登录
+                </Typography.Title>
+              </Row>
+              <Card className={styles.loginCard}>
+                <FormNormalLogin />
+                <JumpSystemLine />
+              </Card>
+            </div>
+          )}
+          {/* 通过日历搜索 */}
+          {calendarSearch && (
+            <>
+              <div className={styles.calendarSearchWrapper}>
+                <Card className={styles.calendarSearchTitle}>
+                  <Row>
+                    <p>
+                      <ContainerOutlined style={{ fontSize: 22, marginRight: 10 }} />
+                      {calendarSearchTime}投标项目
+                    </p>
+                    <p
+                      className={styles.calendarSearchTitleText}
+                      onClick={() => window.location.reload()}
+                    >
+                      {' '}
+                      <DoubleLeftOutlined />
+                      返回
+                    </p>
+                  </Row>
+                  <Divider dashed />
+                </Card>
+                <Card className={styles.calendarSearchContent}>
+                  <ResourceItemElemMore data={purchaseAnnouncement} />
+                </Card>
+              </div>
+            </>
+          )}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default WelcomeMain;
--- src/pages/GuideDetail/index.tsx
+++ src/pages/GuideDetail/index.tsx
@@ -1,51 +1,46 @@
-import Frame from "@/components/Frame";
-import { DownloadOutlined, EyeOutlined } from "@ant-design/icons";
-import styles from './index.less'
+import Frame from '@/components/Frame';
+import { DownloadOutlined, EyeOutlined } from '@ant-design/icons';
+import styles from './index.less';
 
-import { qiNiuUrl } from '../../../config/qiniuyun'
+import { qiNiuUrl } from '../../../config/qiniuyun';
 import FileViewer from 'react-file-viewer';
 import { CustomErrorComponent } from 'custom-error';
-import { Button } from "antd";
-
-
-
+import { Button } from 'antd';
 
 const GuideDetail: React.FC = (props: any) => {
-    const data = props.location?.query
+  const data = props.location?.query;
 
-    const onError = (e: any) => {
-        console.log('显示错误');
-    }
+  const onError = (e: any) => {
+    console.log('显示错误');
+  };
 
-    return (
-        <>
-            <Frame>
-                <div className={styles.contentWrapper}>
-                    <div className={styles.contentTitle}>
-                        <div className={styles.contentTitleText}>
-                            <h3>
-                                {data?.name}
-                            </h3>
-                        </div>
-                        <div className={styles.contentTitleDesc}>
-                            发布时间：{data?.createdAt}
-                        </div>
-                        {data?.type === "download" && <a target="_blank" href={qiNiuUrl + "Resources/" + data?.key}><Button type="dashed" shape="round" icon={<DownloadOutlined />} />点击下载</a>}
-                    </div>
-                    {/* 会有一个bug，资源无法访问的时候会一直 */}
-                    {data?.key ?
-                        <FileViewer
-                            fileType="docx"
-                            filePath={qiNiuUrl + "Resources/" + data?.key}
-                            errorComponent={CustomErrorComponent}
-                            onError={onError} />
-                        : null
-                    }
-                </div>
-            </Frame>
-        </>
-    )
-}
-
+  return (
+    <>
+      <Frame>
+        <div className={styles.contentWrapper}>
+          <div className={styles.contentTitle}>
+            <div className={styles.contentTitleText}>
+              <h3>{data?.name}</h3>
+            </div>
+            <div className={styles.contentTitleDesc}>发布时间：{data?.createdAt}</div>
+          </div>
+          <a target="_blank" href={qiNiuUrl + 'Resources/' + data?.key}>
+            <Button type="dashed" shape="round" icon={<DownloadOutlined />} />
+            点击下载
+          </a>
+          {/* 会有一个bug，资源无法访问的时候会一直 */}
+          {data?.key ? (
+            <FileViewer
+              fileType="docx"
+              filePath={qiNiuUrl + 'Resources/' + data?.key}
+              errorComponent={CustomErrorComponent}
+              onError={onError}
+            />
+          ) : null}
+        </div>
+      </Frame>
+    </>
+  );
+};
 
 export default GuideDetail;
